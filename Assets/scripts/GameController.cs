c0b6f57eae99bc1e88bae60f1a92bde5d71b3126
svc-student <svc-student@student.directory.jccc>
2018-12-06 18:11:30 -0600

    done?

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public RoomGenerator roomGen;
     public PlayerController playerControl;
+    public MainMenuController menuControl;
+    
 
     public float levelTimeLimit = 90f;
 
@@ -15,6 +22,8 @@ public class GameController : MonoBehaviour
     private Text timerText;
     private Text pointsCount;
 
+    public LevelData levelData;
+
     // Use this for initialization
     void Start()
     {
@@ -22,6 +31,8 @@ public class GameController : MonoBehaviour
         playerControl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         timerText = GameObject.FindGameObjectWithTag("Timer").GetComponent<Text>();
         pointsCount = GameObject.FindGameObjectWithTag("PointsCounter").GetComponent<Text>();
+        menuControl = GameObject.Find("UIControl").GetComponent<MainMenuController>();
+        levelData = menuControl.menuLevelData;
     }
 
     // Update is called once per frame
@@ -41,7 +52,43 @@ public class GameController : MonoBehaviour
             //game over
             timerText.text = "0.00";
             playerControl.enabled = false;
-            GameObject.FindGameObjectWithTag("GameOverText").GetComponent<Text>().text = "Game Over. Total Points: " + totalPoints;          
+            
+            GameObject.FindGameObjectWithTag("GameOverText").GetComponent<Text>().text = "Game Over. Total Points: " + totalPoints + "\nPress Escape to Quit";
+            
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SaveScores();
+                Destroy(menuControl.gameObject);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            }
         }
+    }
+
+    private void SaveScores()
+    {
+        levelData.scoreData.Add(totalPoints);
+        levelData.SaveToFile("High Scores");
+    }
+}
+
+//other classes
+
+
+[Serializable]
+public class LevelData
+{
+    public List<int> scoreData = new List<int>();
+    private static string saveDir = Directory.GetCurrentDirectory() + "\\HighScores\\";
+
+    public static LevelData LoadFromFile(string fileName)
+    {
+        string newPath = System.IO.Path.Combine(saveDir, fileName);
+        return JsonUtility.FromJson<LevelData>(System.IO.File.ReadAllText(newPath));
+    }
+
+    public void SaveToFile(string fileName)
+    {
+        string newPath = System.IO.Path.Combine(saveDir, fileName);
+        System.IO.File.WriteAllText(newPath, JsonUtility.ToJson(this, true));
     }
 }
--- Assets/scripts/MainMenuController.cs
+++ Assets/scripts/MainMenuController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    public LevelData menuLevelData = new LevelData();
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,16 +17,37 @@ public class MainMenuController : MonoBehaviour
         Application.Quit();
     }
 
+    public void DisplayHighScores()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
 
-	// Use this for initialization
-	void Start ()
+        menuLevelData = LevelData.LoadFromFile("High Scores");
+
+    }
+    private void OnGUI()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        if (SceneManager.GetActiveScene().name == "HighScores")
+        {
+            for (int i = 0; i < menuLevelData.scoreData.Count; i++)
+            {
+                GUI.Label(new Rect(380, 50 + i * 10, 100, 20), menuLevelData.scoreData[i].ToString());
+            }
+        }
+    }
+    // Use this for initialization
+    void Start()
     {
-		
-	}
+        
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene(0);
+        }
+    }
 }
